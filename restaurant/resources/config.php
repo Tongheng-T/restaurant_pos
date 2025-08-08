c599f65e0db401c109fe78a75d3ccc3473451246
Tongheng-T <tonghengit.com>
2025-08-08 14:27:21 +0700

    ww

--- restaurant/resources/config.php
+++ restaurant/resources/config.php
@@ -10,12 +10,13 @@ defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "temp
 
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 defined("UPLOAD_DIRECTORY_UDER") ? null : define("UPLOAD_DIRECTORY_UDER", "../resources/images/userpic/");
-defined("UPLOAD_DIRECTORY_IDPAY") ? null : define("UPLOAD_DIRECTORY_IDPAY", __DIR__ . DS ."../resources/images/userpay/");
+defined("UPLOAD_DIRECTORY_IDPAY") ? null : define("UPLOAD_DIRECTORY_IDPAY", __DIR__ . DS ."resources/images/userpay");
 
 defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "uploads");
 
 
 // defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", "../resources/");
+
 
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
