59298142648f32ffbccc1cd0a1cf373b7d05bb7f
Tongheng-T <tonghengit.com>
2025-08-17 18:24:21 +0700

    s

--- coffee/resources/functions.php
+++ coffee/resources/functions.php
@@ -1032,7 +1032,7 @@ function addproduct()
             // $txtm_pricee = $txtm_price / $exchange;
         }
 
-        $store = "../productimages/" . $f_newfile;
+        $store = "/www/wwwroot/restaurant_pos/coffee/productimages/" . $f_newfile;
 
         if ($f_extension == 'jpg' || $f_extension == 'jpeg' ||   $f_extension == 'png' || $f_extension == 'gif') {
 
