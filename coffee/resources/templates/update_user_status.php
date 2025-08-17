753cf8ebee1a41d36acb087fc19645d24fbcc4ce
Tongheng-T <87926827+Tongheng-T@users.noreply.github.com>
2025-08-17 20:01:22 +0700

    Delete coffee directory

--- coffee/resources/templates/update_user_status.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../config.php");
-$uid=$_SESSION['userid'];
-$date = new DateTime('now', new DateTimeZone('Asia/bangkok'));
-$datee =  $date->format('Y-m-d H:i:s');
-$time=time()+10;
-$res = query("UPDATE tbl_user set login_online='$time', last_login='$datee' where user_id=" . $_SESSION['userid']);
-confirm($res);
