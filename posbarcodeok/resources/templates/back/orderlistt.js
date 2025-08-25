2a24ab52915cc623b285a628079b4466d7d4f79e
Tongheng-T <tonghengit.com>
2025-08-25 17:31:03 +0700

    s

--- posbarcodeok/resources/templates/back/orderlistt.js
+++ posbarcodeok/resources/templates/back/orderlistt.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
         var tdh = $(this);
         var id = $(this).attr("id");
 
-
         Swal.fire({
             title: 'Do you want to delete?',
             text: "You won't be able to revert this!",
@@ -24,28 +23,38 @@ $(document).ready(function() {
                 $.ajax({
                     url: '../resources/templates/back/ordertdelete.php',
                     type: 'post',
-                    data: {
-                        piddd: id
+                    data: { piddd: id },
+                    success: function(response) {
+                        if (response.includes("success")) {
+                            // hide row
+                            tdh.parents('tr').fadeOut();
+
+                            // show success message
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Invoice has been deleted.',
+                                'success'
+                            );
+                        } else {
+                            Swal.fire(
+                                'Error!',
+                                response,
+                                'error'
+                            );
+                        }
                     },
-                    success: function(data) {
-                        tdh.parents('tr').hide();
+                    error: function(xhr, status, error) {
+                        Swal.fire(
+                            'Error!',
+                            'Failed to delete invoice: ' + error,
+                            'error'
+                        );
                     }
-
-
                 });
 
-
-                Swal.fire(
-                    'Deleted!',
-                    'Your Invoice has been deleted.',
-                    'success'
-                )
             }
-        })
-
-
+        });
     });
-
 });
 
 
