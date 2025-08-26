4827321a0f7f972daeebdc28b919cf28d4c80866
Tongheng-T <tonghengit.com>
2025-08-26 08:37:59 +0700

    c

--- posbarcodeok/resources/config.php
+++ posbarcodeok/resources/config.php
@@ -42,7 +42,7 @@ $details = @file_get_contents("https://ipapi.co/{$ip}/country/");
 $country = trim($details);
 
 // ប្រសិនបើមិនមែនកម្ពុជា (KH) → block
-if ($country !== "KH") {
+if ($country !== "Cambodia") {
     header("HTTP/1.1 403 Forbidden");
     echo "Access denied. This website is only available in Cambodia.$location";
     exit();
