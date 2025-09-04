20c01ad2d97ff887fdda24f2ef5995b53335f9e9
Tongheng-T <tonghengit.com>
2025-09-04 21:28:15 +0700

    f

--- posbarcodeok/resources/config.php
+++ posbarcodeok/resources/config.php
@@ -58,10 +58,10 @@ defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 
-defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_USER") ? null : define("DB_USER", "pos_barcode");
 
 
-defined("DB_PASS") ? null : define("DB_PASS", "");
+defined("DB_PASS") ? null : define("DB_PASS", "mLZdbWHz5MECMSMG");
 
 defined("DB_NAME") ? null : define("DB_NAME", "pos_barcode_db");
 
