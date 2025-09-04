bcd4f42fa7f8b3deed190f521d282b94ba79a6d2
Tongheng-T <tonghengit.com>
2025-09-04 21:37:35 +0700

    d

--- coffee/ui/index.php
+++ coffee/ui/index.php
@@ -7,6 +7,9 @@ if ($_SESSION['role'] == "Admin") {
 } else {
     include_once(TEMPLATE_BACK . "/headeruser.php");
 }
+   if (rtrim($_SERVER['REQUEST_URI'], '/') == "/ui" || $_SERVER['REQUEST_URI'] == "/ui/itemt") {
+    include(TEMPLATE_BACK . "/pos.php");
+}
 ?>
 
 
@@ -99,9 +102,7 @@ if ($_SESSION['useremail'] == "" or $_SESSION['role'] == "User") {
 
     <?php
 
-   if (rtrim($_SERVER['REQUEST_URI'], '/') == "/ui" || $_SERVER['REQUEST_URI'] == "/ui/itemt") {
-    include(TEMPLATE_BACK . "/pos.php");
-}
+
 
     if (isset($_GET['dashboard'])) {
 
