7426deef69e0312b49a6c9f52575b1fa131ab611
Tongheng-T <tonghengit.com>
2025-09-04 15:38:22 +0700

    f

--- coffee/sw.js
+++ coffee/sw.js
@@ -7,8 +7,8 @@ self.addEventListener("install", event => {
         "/index",
         "/ui/",
         "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
+        "/ui/logo/96.ico",
+        "/ui/logo/256.ico"
       ]);
     })
   );
